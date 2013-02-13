6694662860101a10735915d184b14950c382f713
Laurence Blaxter <Laurence.blaxter@btinternet.com>
2013-02-13 21:13:13 +0000

    Changes to CCMRAM init

--- late_init.c
+++ late_init.c
@@ -3,6 +3,7 @@
 extern uint32_t _siccmram,_sccmram,_eccmram;
 
 void __late_init(void) {
-	for(uint32_t addr=_sccmram,uint32_t addr_flash=_siccmram; addr<=_eccmram: addr+=4, addr_flash+=4 )
-		*(uint32_t*)addr = *(uint32_t*)addr_flash;
+	uint32_t *addr = &_sccmram, *addr_flash = &_siccmram;
+	while( addr < &_eccmram)
+		*addr++ = *addr_flash++;
 }
